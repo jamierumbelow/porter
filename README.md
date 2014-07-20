dcf4e6f90424ff024ffeac69b68f50296317c7b3
Jamie Rumbelow <jamie@jamierumbelow.net>
2014-07-20 19:57:19 +0100

    adding basic trait

---
+++ README.md
@@ -0,0 +1,63 @@
+# Porter
+
+Porter is a lightweight, framework-agnostic authorization solution for PHP applications, loosely based on [CanCan](https://github.com/ryanb/cancan). It requires PHP5.3, but other than that makes no assumptions about your framework or database structure.
+
+## Quickstart (Laravel)
+
+After [installing](#installation), inside your `User` model:
+
+```php
+use JR\Porter\User as Porter;
+
+class User extends Eloquent
+{
+    use Porter;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        UserAbilities::setup($this);
+    }
+}
+```
+
+...and inside a separate `UserAbilities` class:
+
+```php
+class UserAbilities
+{
+    public static function setup(User $user)
+    {
+        $user->may('read', 'Book', function($b)
+        {
+            return ($b->private) ? ( $b->author_id == $this->id ) : TRUE;
+        });
+
+        $user->mayIfEquals('manage', 'Book', 'author_id', $user->id);
+        $user->mayIfEquals('manage', 'User', 'id', $user->id);
+    }
+}
+```
+
+And then, in your app:
+
+```php
+$user = Auth::user();
+$book = Book::find($bookId);
+
+if ($user->can('manage', $book))
+{
+    $book->fill(Input::get('book'));
+
+    // et cetera
+}
+```
+
+## Philosophy
+
+## Installation
+
+## Usage
+
+## License
---
+++ src/User.php
@@ -0,0 +1,82 @@
+<?php namespace JR\Porter;
+
+trait User
+{
+    protected $_abilities = array();
+    protected $_overlord = FALSE;
+
+    public function may($action, $thing, callable $callback = null)
+    {
+        // Do we have a record of $thing already?
+        if (!isset($this->_abilities[$thing]))
+            $this->_abilities[$thing] = array();
+
+        // Do we have a callback? If not, just use TRUE (bool).
+        $this->_abilities[$thing][$action] = $callback ?: TRUE;
+    }
+
+    public function mayIfEquals($action, $thing, $what, $compareValue)
+    {
+        $this->may($action, $thing, function($instance) use ($what, $compareValue)
+        {
+            return ($instance->$what == $compareValue);
+        });
+    }
+
+    public function can($action, $thing)
+    {
+        if ($this->_overlord === TRUE) return TRUE;
+
+        $result = FALSE;
+            
+        // $thing is likely to be an instance of a specific class
+        if (is_object($thing))
+            $class = get_class($thing);
+        else
+            $class = $thing;
+
+        if (isset($this->_abilities[$class]) && isset($this->_abilities[$class][$action]))
+        {
+            // If we have specified a callback, we want to pass it whatever $thing is
+            // and use the return value of that as our result. This lets us have custom
+            // callback code on a per-instance basis.
+            if (is_callable($this->_abilities[$class][$action]))
+            {
+                $result = call_user_func_array($this->_abilities[$class][$action], array( $thing ));
+            }
+
+            // Else, just check to see if it's obvious that we're allowed to do something.
+            else
+            {
+                $result = ($this->_abilities[$class][$action] === TRUE);
+            }
+        }
+
+        // We've not managed to find this specific action. Maybe we need to check for 'manage'?
+        elseif ( isset($this->_abilities[$class]) && ( $action == 'edit' || $action == 'create' ) && ( isset($this->_abilities[$class]['manage']) ) )
+        {
+            if (is_callable($this->_abilities[$class]['manage']))
+                $result = call_user_func_array($this->_abilities[$class]['manage'], array( $thing ));
+            else
+                $result = ($this->_abilities[$class]['manage'] === TRUE);
+        }
+
+        // Nope, we've got nothing. Better not let them do it, then!
+        else
+        {
+            $result = FALSE;
+        }
+
+        return $result;
+    }
+
+    public function cannot($action, $thing)
+    {
+        return !$this->can($action, $thing);
+    }
+
+    public function isOverlord()
+    {
+        $this->_overlord = TRUE;
+    }
+}
